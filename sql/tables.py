04d631fda81c095d1f7cccc8a716e508962f465b
AlexanderGreyFox <alexserov0@gmail.com>
2022-05-22 19:08:48 +0300

    Create tables for sql db

--- sql/tables.py
+++ sql/tables.py
@@ -1,13 +1,51 @@
-from sqlalchemy import MetaData, Table, String, Integer, Column, Text, DateTime, Boolean, Float
+from sqlalchemy import create_engine, Integer, String, \
+    Column, ForeignKey, Float, Boolean, ARRAY, Text
 
-metadata = MetaData()
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
-ingredients = Table("ingredients", metadata,
-                    Column('id', Integer, primary_key=True),
-                    Column('en_name', String(200), nullable=False),
-                    Column('ru_name', String(200), nullable=False),
-                    Column('kcal', Float, nullable=False),
-                    Column('protein', Float, nullable=False),
-                    Column('carbohydrate', Float, nullable=False),
-                    Column('fat', Float, nullable=False)
-                    )
+engine = create_engine("postgresql+psycopg2://postgres:1111@localhost/sqlalchemy_tuts")
+
+Base = declarative_base()
+
+
+class Ingredients(Base):
+    __tablename__ = "ingredient"
+    id = Column(Integer(), primary_key=True)
+    en_name = Column(String(200), nullable=False)
+    ru_name = Column(String(200), nullable=False)
+    kcal = Column(Float(), nullable=False),
+    protein = Column(Float(), nullable=False),
+    carbohydrate = Column(Float(), nullable=False)
+    fat = Column(Float(), nullable=False)
+    tablespoon = Column(Integer(), nullable=False)
+    cup = Column(Integer(), nullable=False)
+    vegetarian = Column(Boolean(), nullable=False)
+    recipe = relationship("IngredientsInRecipe", backref='ingredient')
+
+
+class Recipes(Base):
+    __tablename__ = "recipe"
+    id = Column(Integer(), primary_key=True)
+    ru_name = Column(String(200), nullable=False)
+    en_name = Column(String(200), nullable=False)
+    kcal = Column(Float(), nullable=False),
+    protein = Column(Float(), nullable=False),
+    carbohydrate = Column(Float(), nullable=False)
+    fat = Column(Float(), nullable=False)
+    ingredients_ru = Column(ARRAY(String))
+    ingredients_en = Column(ARRAY(String))
+    description = Column(Text(), nullable=False)
+    vegetarian = Column(Boolean(), nullable=False)
+    ingredient = relationship("IngredientsInRecipe", backref='recipe')
+
+
+class IngredientsInRecipe(Base):
+    __tablename__ = "ingredients_in_recipe"
+    id = Column(Integer, primary_key=True)
+    recipe = Column(Integer(), ForeignKey("recipe.id"))
+    ingredient = Column(Integer(), ForeignKey("ingredient.id"))
+    extra_data = Column(String(100))
+
+
+Base.metadata.create_all(engine)
